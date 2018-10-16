e1ada6dca1aad436cf885e30f1900440cbf8562a
Wawoul <ewen.paterson@gmail.com>
2018-10-16 23:00:41 +0100

    MAX7219 Muliple Toggle Buttons
    Using 3 buttons to toggle images on each of MAX7219 matrix modules

--- Leds/Test Sketches/MAX7219_32x8_MultiButton_Test/MAX7219_32x8_MultiButton_Test.ino
+++ Leds/Test Sketches/MAX7219_32x8_MultiButton_Test/MAX7219_32x8_MultiButton_Test.ino
@@ -4,17 +4,28 @@ https://anu.biz/
 16/10/2018
 
 ---------------------------------------------------------------------------
-Ardunio will display faces on each button press 
+Ardunio will display faces on different switch buttons that are toggled 
 Displaying on 32x8 MAX7219 Matrix Module 4-in-1 Display. 
+
+Note: only using 3 buttons for module 1,2,3. 4 is unused
 ---------------------------------------------------------------------------
 */
 
 #include "LedControl.h"
 #include "binary.h"
-const int buttonPin = 3; 
-int counter = 0;
+const int hButtonPin = 7; //First button (Happy)
+const int nButtonPin = 6; //Sceond button (Neutral)
+const int sButtonPin = 5; //Third button (Sad)
 
-byte max_units = 4; //4 as we are only using 4 MAX7219
+int hButtonState; // variable for reading the pushbutton status
+int nButtonState;
+int sButtonState;  
+int hFlag = 0; //flag when button is toggled
+int nFlag = 0;
+int sFlag = 0;
+
+byte max_units = 4; 
+//4 as we are only using 4 MAX7219
 LedControl lc = LedControl(12,11,10,max_units);
 //DOUT connects to pin 12
 //CLK connects to pin 11
@@ -24,7 +35,7 @@ LedControl lc = LedControl(12,11,10,max_units);
 
 // Delay time between faces:
 //-------------------------------
-unsigned long delaytime=1000;
+//unsigned long delaytime=1000;
 
 // Happy face:
 byte ff[8]= {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
@@ -53,7 +64,9 @@ void setup() {
   lc.clearDisplay(3);
 
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(hButtonPin, INPUT);
+  pinMode(nButtonPin, INPUT);
+  pinMode(sButtonPin, INPUT);     
 } 
 
 void happyface() {
@@ -89,47 +102,55 @@ void sadface()  {
   lc.setRow(2,7,f2[7]);
 }
 
-void miscface() {
-  lc.setRow(3,0,f3[0]);
-  lc.setRow(3,1,f3[1]);
-  lc.setRow(3,2,f3[2]);
-  lc.setRow(3,3,f3[3]);
-  lc.setRow(3,4,f3[4]);
-  lc.setRow(3,5,f3[5]);
-  lc.setRow(3,6,f3[6]);
-  lc.setRow(3,7,f3[7]);
-}
+//void miscface() {
+//  lc.setRow(3,0,f3[0]);
+//  lc.setRow(3,1,f3[1]);
+//  lc.setRow(3,2,f3[2]);
+//  lc.setRow(3,3,f3[3]);
+//  lc.setRow(3,4,f3[4]);
+//  lc.setRow(3,5,f3[5]);
+//  lc.setRow(3,6,f3[6]);
+//  lc.setRow(3,7,f3[7]);
+//}
 
 void loop() {
-  int buttonState;
-  buttonState = digitalRead(buttonPin);
-  
-  if (buttonState == HIGH) {
-    counter++;
-    delay(150);
-  }
-  
-  if (counter == 1) {
+  // read the state of the pushbutton value:
+  hButtonState = digitalRead(hButtonPin);
+  nButtonState = digitalRead(nButtonPin);
+  sButtonState = digitalRead(sButtonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (hButtonState == HIGH && hFlag == 0) {     
     happyface();
+    hFlag = 1;
+    hButtonState == LOW;
+    delay(500); //small delay to account for button bounce.
+  } else if (hButtonState == HIGH && hFlag == 1) {
+     lc.clearDisplay(0);
+     hFlag = 0;
+     delay(500);
   }
   
-  if (counter == 2) {
+  if (nButtonState == HIGH && nFlag == 0) {     
     neturalface();
+    nFlag = 1;
+    nButtonState == LOW;
+    delay(500); //small delay to account for button bounce.
+  } else if (nButtonState == HIGH && nFlag == 1) {
+     lc.clearDisplay(1);
+     nFlag = 0;
+     delay(500);
   }
-  
-  if (counter == 3) {
+
+   if (sButtonState == HIGH && sFlag == 0) {     
     sadface();
-  }
-  
-  if (counter == 4) {
-    miscface();
-  }
-  
-  if (counter == 5) {
-    lc.clearDisplay(0);
-    lc.clearDisplay(1);
-    lc.clearDisplay(2);
-    lc.clearDisplay(3);
-    counter = 0;
+    sFlag = 1;
+    sButtonState == LOW;
+    delay(500); //small delay to account for button bounce.
+  } else if (sButtonState == HIGH && sFlag == 1) {
+     lc.clearDisplay(2);
+     sFlag = 0;
+     delay(500);
   }
 }
